6d1dce93ce4487a4979f1ecfd3da6eef95186b9d
kama04 <kamilla.gorobets0409@gmail.com>
2022-02-09 23:40:29 +0200

    second

---
+++ task1.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 3;
+$b = 4;
+$c = 5;
+if ($a > $b && $a > $c) {
+    echo "a большее";
+} else if ($b > $a && $b > $c) {
+    echo "b большее";
+} else {
+    echo "c большее";
+}
+
+if ($a < $b && $a < $c) {
+    echo " a меньшее";
+} else if ($b < $a && $b < $c) {
+    echo " b меньшее";
+} else {
+    echo " c меньшее";
+}
